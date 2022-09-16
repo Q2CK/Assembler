3d88728bc76d169d486e5d777def32723c6aa053
Q2CK <109765887+Q2CK@users.noreply.github.com>
2022-09-16 12:01:57 +0200

    define and isa declaration

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
 import json
+import os
+from types import SimpleNamespace
 
-allowed_define_chars = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz0123456789"]
+here = os.path.dirname(os.path.abspath(__file__))
 
 
 class Assembler:
-    def __init__(self):
-        self.asm_file = open(input("Assembly file name: "), "r")
 
-        self.ISA: list[str] = []
+    def __init__(self) -> None:
+
+        self.isa: object = None
+        self.isa_file_name: list[str] = []
         self.define: dict = {}
 
-    def read_lines(self):
+        error: bool = True
+
+        while error:
+            try:
+                asm_filename = input("Assembly file name: ")
+                self.asm_file = open(os.path.abspath(asm_filename), "r")
+
+            except FileNotFoundError:
+                print("File not found")
+                error = False
+                self.asm_file.close()
+
+            else:
+                error, asm_lines = self.find_declarations()
+                self.assemble()
+
+    def assemble(self):
+        self.isa = json.loads(
+            open(os.path.abspath(f"ISA/{self.isa_file_name[0]}"), "r").read().lower(),
+            object_hook=lambda d: SimpleNamespace(**d))
+
+        print(self.isa.instructions.add.opcode)
+
+    def read_lines(self) -> list[str]:
         asm_lines: list[str] = []
         asm_line: str = self.asm_file.readline()
 
@@ -20,28 +46,51 @@ class Assembler:
 
         return asm_lines
 
-    def find_declarations(self):
+    def find_declarations(self) -> tuple[bool, list[str]]:
         asm_lines = self.read_lines()
+        output_lines: list[str] = []
 
         for line in asm_lines:
-            tokens = line.split()
+            tokens: list[str] = line.split()
 
-            if tokens[0] in ["#isa", "#ISA", "#Isa"] and len(tokens) == 2:
-                self.ISA.append(tokens[1] + ".json")
+            if len(tokens) == 2 and tokens[0] in ["#isa", "#ISA", "#Isa"] :
+                self.isa_file_name.append(tokens[1] + ".json")
 
-            elif tokens[0] in ["#DEFINE", "#Define", "#define"] and len(tokens) == 3:
-                self.define[tokens[1]] = tokens[2]
+            elif len(tokens) == 3 and tokens[0] in ["#DEFINE", "#Define", "#define"]:
 
-        if len(self.ISA) > 1:
+                errors: list[str] = []
+
+                for char in tokens[1]:
+                    if not char.isalnum():
+                        errors.append(char)
+
+                for char in tokens[2]:
+                    if not char.isalnum():
+                        errors.append(char)
+
+                if len(errors) == 0:
+                    self.define[tokens[1]] = tokens[2]
+
+                else:
+                    print(f"Chars '{errors}' are not allowed in define statements")
+                    return True, []
+
+            elif len(tokens) == 0:
+                pass
+
+            else:
+                output_lines.append(line)
+
+        if len(self.isa_file_name) > 1:
             print("Single ISA file declaration required")
-            return False
+            return True, []
 
-        elif len(self.ISA) < 1:
-            print("Single ISA file declaration required")
-            return False
+        elif len(self.isa_file_name) < 1:
+            print("ISA file declaration required")
+            return True, []
 
         else:
-            return True
+            return False, output_lines
 
 
 assembler: Assembler = Assembler()
---
+++ test.asm
@@ -0,0 +1,7 @@
+#define bajo 3
+#define jajo 4
+#isa AnPU2
+
+lda $2
+add $3
+
